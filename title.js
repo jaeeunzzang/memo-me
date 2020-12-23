58889a26ca4ca9f5764df1b60d9c3ae2cf5cf89c
jenny <qkrwodms0201@gmail.com>
2020-12-23 14:31:40 +0900

    title js,css

---
+++ title.js
@@ -0,0 +1,17 @@
+var memo = document.getElementById("memo");
+var memome = document.getElementById("memome");
+var aTag = memome.children[0];
+// console.log(aTag);
+
+memo.onmouseover = memoHover;
+memo.onmouseleave = memoHout;
+
+function memoHover() {
+  memo.style.display = "none";
+  memome.style.display = "block";
+  aTag.style.color = "pink";
+}
+function memoHout() {
+  memo.style.display = "block";
+  memome.style.display = "none";
+}
