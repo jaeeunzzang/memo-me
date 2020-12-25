2037435f503b8e390891ad011d0e07b5ccf9165d
jenny <qkrwodms0201@gmail.com>
2020-12-25 14:37:57 +0900

    javascript update

---
+++ index.js
@@ -0,0 +1,7 @@
+var recipe = document.getElementById("recipe");
+var article = document.getElementById("article");
+recipe.onclick = goRecipe;
+function goRecipe() {
+  console.log("click");
+  document.location.href = "recipe.html";
+}
